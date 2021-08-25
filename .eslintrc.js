36949b31c578b41b10a5de12536520f552e6813f
ukigumo-shiina3 <daisukeyao@outlook.jp>
2021-08-25 22:43:59 +0900

    ✨ ESLintの細かい設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,24 @@
-module.exports = {
+module.exports = { //
     "env": {
-        "browser": true,
-        "es2021": true
+        "browser": true, // documentが元々定義されていると認識させる
+        "es2021": true, //最新のJSの機能を認識させる
+        "node": true // moduleなどのnodeのグローバル変数を認識させる
     },
-    "extends": "plugin:react/recommended",
+    "extends": "plugin:react/recomended", // rulesのデフォルトのセットを決める。recommendの場合、パッケージ側が用意しているおすすめの設定を使える
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
         },
-        "ecmaVersion": 12,
         "sourceType": "module"
-    },
+    }, // parserOptions: 最新の構文を解析できるようになる
     "plugins": [
         "react"
-    ],
+    ], // plugins: rulesに独自の設定を追加する
     "rules": {
-        "semi": ["error"], // "semi": ["error", "always"]でもかける。「"always"」がデフォルトのため省略できる
-        "quotes": ["error"],  // "quotes": ["error", "double"]でもかける。「"double"」がデフォルトのため省略できる
-        "react/prop-types": ["off"] // 「error, warn, off」の中から表記を選択できる
+        "semi": "error", // "semi": ["error", "always"]でもかける。「"always"」がデフォルトのため省略できる
+        "quotes": "error",  // "quotes": ["error", "double"]でもかける。「"double"」がデフォルトのため省略できる
+        "react/prop-types": "off", // 「error, warn, off」の中から表記を選択できる
+        "no-undef": "error",
+        "react/jsx-props-no-spreading": "off",
     }
 };
